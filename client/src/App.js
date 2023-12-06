f43303d91d19f3107517e8c34b46bbee5786b9d2
Nick Shryock <n.shryock10@gmail.com>
2023-12-06 11:19:45 -0500

    adjusted git path

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Components/Home';
+import Practice from './Components/Practice';
+import Review from './Components/Review';
+import Nav from './Components/Nav';
+import {BrowserRouter as Router, Route, Routes, useSearchParams} from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { getAnswer, getLegend } from './utils/utils';
+
+function App() {
+
+  const [legend, setLegend] = useState(null);
+
+  useEffect(() => {
+    
+  }, [])
+
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route exact path='/practice' element={<Practice />} />
+          <Route exact path='/review' element={<Review />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
