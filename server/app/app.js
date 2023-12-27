56ae40ccda3489d919f8a6690650185273132270
Nick Shryock <n.shryock10@gmail.com>
2023-12-27 13:34:28 -0500

    added path to server

--- server/app/app.js
+++ server/app/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = require('./routes');
+const path = require('path');
 const logger = require('morgan');
 const cors = require('cors');
 
@@ -12,6 +13,8 @@ app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 app.use('/', router);
 
