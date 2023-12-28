65e7eb40a89ae4021b82daab13a27f62b353c7ed
Nick Shryock <n.shryock10@gmail.com>
2023-12-28 14:09:15 -0500

    adjusted wildcard route

--- server/app/app.js
+++ server/app/app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, '../.././client', 'build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../.././client/public', 'index.html'))
+    res.sendFile(path.join(__dirname, 'client/public', 'index.html'))
 })
 
 app.use('/', router);
